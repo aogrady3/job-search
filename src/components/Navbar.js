07ef5c7e67cdaaa74be6fad8009334596cbf0e28
Andrew O'Grady <andrewogrady3@gmail.com>
2019-11-22 18:10:01 -0500

    style added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
+import "../css/Navbar.css"
+
 const Navbar = () => {
     return (
-    <div>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
+    <div className='navbar'>
+        <div className='title'>
+            <h1>JOB SEARCH APPLICATION</h1>
+        </div>
+        <div className='links'>
+            <Link to='/'>
+                <h1>Home</h1>
+                </Link> 
+            <Link to='/about'>
+                <h1>About</h1>
+                </Link>
+        </div>
+
     </div>
     )
 }
