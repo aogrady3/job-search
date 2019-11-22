c96bb6228632434ae961be7d987a6438745ad48d
Andrew O'Grady <andrewogrady3@gmail.com>
2019-11-22 15:34:05 -0500

    added some style;

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,9 +1,14 @@
 import React from 'react' 
 
-const Job = () => {
+//import CSS
+import '../css/Job.css'
+
+const Job = (props) => {
+    const job = props.job
     return (
-        <div>
-            Single Job
+        <div className = 'single-job-post'>
+            <img src={job.company_logo} />
+            <h3>{job.company}</h3>
         </div>
     )
 }
